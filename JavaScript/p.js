4ddd7e54ce7fb656816db509aa554145063fbfab
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-01-31 04:03:19 +0600

    operations

--- JavaScript/p.js
+++ JavaScript/p.js
@@ -1,3 +1,21 @@
-var age = 23;
-console.log(name1);
-var name1 = "Ahanf Akib"
+// var age = 23;
+// console.log(age);
+// var name1 = "Ahanf Akib"
+// console.log(name1);
+
+// var price = 87;
+// console.log(price);
+
+// console.log("Paw's Step");
+
+var eggPrice = 11;
+var mangoPrice = 3;
+var totalPrice = eggPrice + mangoPrice;
+console.log(totalPrice);
+var priceDifference = (eggPrice - mangoPrice);
+console.log(priceDifference);
+var priceMultipication = (eggPrice * mangoPrice);
+var divitionOfPrice = (eggPrice / mangoPrice);
+
+
+console.log(totalPrice, priceDifference, priceMultipication, divitionOfPrice);
