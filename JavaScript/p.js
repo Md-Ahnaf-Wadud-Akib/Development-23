7adc93e6ac94710041e7463114c743751b465367
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-01-31 04:23:27 +0600

    names

--- JavaScript/p.js
+++ JavaScript/p.js
@@ -8,14 +8,19 @@
 
 // console.log("Paw's Step");
 
-var eggPrice = 11;
-var mangoPrice = 3;
-var totalPrice = eggPrice + mangoPrice;
-console.log(totalPrice);
-var priceDifference = (eggPrice - mangoPrice);
-console.log(priceDifference);
-var priceMultipication = (eggPrice * mangoPrice);
-var divitionOfPrice = (eggPrice / mangoPrice);
+// var eggPrice = 11;
+// var mangoPrice = 3;
+// var totalPrice = eggPrice + mangoPrice;
+// console.log(totalPrice);
+// var priceDifference = (eggPrice - mangoPrice);
+// console.log(priceDifference);
+// var priceMultipication = (eggPrice * mangoPrice);
+// var divitionOfPrice = (eggPrice / mangoPrice);
 
 
-console.log(totalPrice, priceDifference, priceMultipication, divitionOfPrice);
+// console.log(totalPrice, priceDifference, priceMultipication, divitionOfPrice);
+
+
+var firstName = "Ahanf"
+var lastName = "Akib"
+console.log(firstName + " " + lastName);
