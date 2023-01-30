fc3880772fd59f6ec61a2f2da8e1699f29d52c0f
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-01-31 04:47:18 +0600

    typeOf

--- JavaScript/stringToNumber.js
+++ JavaScript/stringToNumber.js
@@ -1,7 +1,13 @@
-var phoneNumber = "4763786";
-var phoneNumberInt = parseInt(phoneNumber);
-console.log(phoneNumberInt);
+// var phoneNumber = "4763786";
+// var phoneNumberInt = parseInt(phoneNumber);
+// console.log(phoneNumberInt);
 
-var eggPrice = "11.23";
-var eggPriceFloat = parseFloat(eggPrice);
-console.log(eggPriceFloat);
+// var eggPrice = "11.23";
+// var eggPriceFloat = parseFloat(eggPrice);
+// console.log(eggPriceFloat);
+
+var productName = "egg";
+var productPrice = 30;
+var productNameType = typeof productName;
+var productPriceType = typeof productPrice;
+console.log(productName + " is a " + productNameType + " and it's price is " + productPrice + " and it is a " + productPriceType);
