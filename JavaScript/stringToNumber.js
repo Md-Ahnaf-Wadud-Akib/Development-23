498a3a2abd57653b83176a16f93ea8b581dee1fb
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-01-31 05:03:22 +0600

    toFixed

--- JavaScript/stringToNumber.js
+++ JavaScript/stringToNumber.js
@@ -6,8 +6,18 @@
 // var eggPriceFloat = parseFloat(eggPrice);
 // console.log(eggPriceFloat);
 
-var productName = "egg";
-var productPrice = 30;
-var productNameType = typeof productName;
-var productPriceType = typeof productPrice;
-console.log(productName + " is a " + productNameType + " and it's price is " + productPrice + " and it is a " + productPriceType);
+// var productName = "egg";
+// var productPrice = 30;
+// var productNameType = typeof productName;
+// var productPriceType = typeof productPrice;
+// console.log(productName + " is a " + productNameType + " and it's price is " + productPrice + " and it is a " + productPriceType);
+
+var num1 = 2.3, num2 = 1.9, num3 = 0.6;
+var total = num1 + num2 + num3;
+console.log(total);
+
+total = total.toFixed(2);
+console.log(total + " is a string");
+
+total = parseFloat(total);
+console.log(total);
