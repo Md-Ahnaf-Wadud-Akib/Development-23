4963280558ce52b9548d044966f392c1db7c5029
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 04:24:23 +0600

    function

---
+++ JavaScript/function.js
@@ -0,0 +1,41 @@
+// function say() {
+//     console.log("Let's have some tea");
+//     console.log("Or maybe Coffee");
+// }
+
+// say();
+
+// function foodValue(taka) {
+//     if (taka == 0) {
+//         console.log("gave me no money to bring the food");
+//     }
+//     else {
+//         console.log("gave me ", taka, " to bring the food");
+//     }
+// }
+
+// foodValue(100);
+// foodValue(150);
+// foodValue(0);
+
+function result(marks) {
+    if (marks > 80) {
+        console.log("your result is A+");
+    }
+    else if (marks > 70) {
+        console.log("Youer result is A");
+    }
+    else if (marks > 60) {
+        console.log("your result is A-");
+    }
+    else {
+        console.log("Your result is laddu-guddu");
+    }
+}
+
+var student1 = 62;
+var student2 = 42;
+console.log("result of student1 is :");
+result(student1);
+console.log("result of student2 is :");
+result(student2);
