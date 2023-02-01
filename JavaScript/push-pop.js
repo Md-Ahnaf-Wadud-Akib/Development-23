0d526d264984886cb0c911073d21ee8ccbd22ef0
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 01:10:09 +0600

    LilyAdded

---
+++ JavaScript/push-pop.js
@@ -0,0 +1,16 @@
+var vu = ["Ahanf", "Akib"]
+console.log(vu);
+vu.push("Fahmida", "Rahman");
+console.log(vu);
+
+vu.push("Oreo the cat");
+console.log(vu);
+
+vu.push("Lily the cutie");
+console.log(vu);
+
+vu.pop();
+console.log(vu);
+
+vu.pop()
+console.log(vu);
