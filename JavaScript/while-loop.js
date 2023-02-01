10d62bbf09721d648939577c9aedb8efaab1464e
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 03:22:34 +0600

    array

--- JavaScript/while-loop.js
+++ JavaScript/while-loop.js
@@ -4,8 +4,22 @@
 //     days++;
 // }
 
-var i;
-for (i = 0; i <= 10; i += 2) {
-    console.log(i);
-    console.log("i am happy");
+// var i;
+// for (i = 0; i <= 10; i += 2) {
+//     console.log(i);
+//     console.log("*");
+// }
+
+// var cats = ["Oreo", "Covid", "Lily", "Timtim"], i;
+
+// for (i = 0; i <= 3; i++) {
+//     catNumber = cats[i];
+//     console.log(catNumber);
+// }
+
+var num1 = [23, 34, 45, 56, 67, 78, 89, 90], i;
+
+for (i = 0; i <= 7; i++) {
+    indexNum1 = num1[i];
+    console.log(indexNum1);
 }
