1840474a23d50b3518170281267dc4a626193b98
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 01:46:00 +0600

    Oreo

--- JavaScript/push-pop.js
+++ JavaScript/push-pop.js
@@ -1,16 +1,54 @@
-var vu = ["Ahanf", "Akib"]
-console.log(vu);
-vu.push("Fahmida", "Rahman");
-console.log(vu);
+// // var vu = ["Ahanf", "Akib"]
+// // console.log(vu);
+// // vu.push("Fahmida", "Rahman");
+// // console.log(vu);
 
-vu.push("Oreo the cat");
-console.log(vu);
+// // vu.push("Oreo the cat");
+// // console.log(vu);
 
-vu.push("Lily the cutie");
-console.log(vu);
+// // vu.push("Lily the cutie");
+// // console.log(vu);
 
-vu.pop();
-console.log(vu);
+// // vu.pop();
+// // console.log(vu);
 
-vu.pop()
-console.log(vu);
+// // var lastpop = vu.pop()
+// // console.log(vu);
+// // console.log(lastpop);
+
+// var edu = ["City", "Vu"];
+// console.log(edu);
+// edu.shift();
+// console.log(edu);
+
+// edu.unshift("MMA");
+// console.log(edu);
+
+// if (5 == 4) {
+//     console.log("Oreo is a loving cat");
+
+// }
+// else {
+//     console.log("Oreo is a food");
+// }
+
+// var a = 2, b = 4, c = 6;
+
+// if (a + a == c && c - a == b) {
+//     console.log("the statements are true");
+// }
+// else if (a + b == c || a + c == b) {
+//     console.log("one of the statements are true");
+// }
+// else {
+//     console.log("the statements are wrong");
+// }
+
+var cat = "Oreo";
+
+if (cat != "Oreo") {
+    console.log("Oreo is a cat");
+}
+else {
+    console.log("Oreo is a food");
+}
