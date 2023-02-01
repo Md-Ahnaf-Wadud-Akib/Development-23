016218d58490723995ad228dd8a3c64f8556da48
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 02:41:40 +0600

    added

--- JavaScript/while-loop.js
+++ JavaScript/while-loop.js
@@ -1,5 +1,11 @@
-var days = 0;
-while (days == 5) {
-    console.log("You have won");
-    days++;
+// var days = 0;
+// while (days < 5) {
+//     console.log("You have won");
+//     days++;
+// }
+
+var i;
+for (i = 0; i <= 10; i += 2) {
+    console.log(i);
+    console.log("i am happy");
 }
