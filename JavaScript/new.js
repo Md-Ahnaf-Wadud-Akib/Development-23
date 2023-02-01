7425aa42a6ebdd2581a417529994726a5d15ab76
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 01:01:35 +0600

    new.js

---
+++ JavaScript/new.js
@@ -0,0 +1,20 @@
+// var cats = ["Oreo", "Lily", "Covid", "Timtim"];
+
+// console.log(cats.indexOf("Covid"));
+
+// var num = ["1", "2", "3"];
+
+// numValue = parseInt(num);
+// console.log(num.indexOf("2"));
+
+var names = ["Neko", "Putu", "Maw"];
+
+console.log(names);
+names[2] = "Oreo";
+console.log(names);
+
+// var num1 = [23, 34, 45, 56, 67];
+// console.log(num1);
+
+// num1[2] = 43;
+// console.log(num1); 
