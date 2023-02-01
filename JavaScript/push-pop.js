0b8cf8c9f3aee1b4c1bb281cebb98f08603ca8b2
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-02 02:35:40 +0600

    while-loop

--- JavaScript/push-pop.js
+++ JavaScript/push-pop.js
@@ -1,56 +1,71 @@
-// // var vu = ["Ahanf", "Akib"]
-// // console.log(vu);
-// // vu.push("Fahmida", "Rahman");
-// // console.log(vu);
+// // // var vu = ["Ahanf", "Akib"]
+// // // console.log(vu);
+// // // vu.push("Fahmida", "Rahman");
+// // // console.log(vu);
 
-// // vu.push("Oreo the cat");
-// // console.log(vu);
+// // // vu.push("Oreo the cat");
+// // // console.log(vu);
 
-// // vu.push("Lily the cutie");
-// // console.log(vu);
+// // // vu.push("Lily the cutie");
+// // // console.log(vu);
 
-// // vu.pop();
-// // console.log(vu);
+// // // vu.pop();
+// // // console.log(vu);
 
-// // var lastpop = vu.pop()
-// // console.log(vu);
-// // console.log(lastpop);
+// // // var lastpop = vu.pop()
+// // // console.log(vu);
+// // // console.log(lastpop);
 
-// var edu = ["City", "Vu"];
-// console.log(edu);
-// edu.shift();
-// console.log(edu);
+// // var edu = ["City", "Vu"];
+// // console.log(edu);
+// // edu.shift();
+// // console.log(edu);
 
-// edu.unshift("MMA");
-// console.log(edu);
+// // edu.unshift("MMA");
+// // console.log(edu);
 
-// if (5 == 4) {
-//     console.log("Oreo is a loving cat");
+// // if (5 == 4) {
+// //     console.log("Oreo is a loving cat");
 
+// // }
+// // else {
+// //     console.log("Oreo is a food");
+// // }
+
+// // var a = 2, b = 4, c = 6;
+
+// // if (a + a == c && c - a == b) {
+// //     console.log("the statements are true");
+// // }
+// // else if (a + b == c || a + c == b) {
+// //     console.log("one of the statements are true");
+// // }
+// // else {
+// //     console.log("the statements are wrong");
+// // }
+
+// var cat = "Oreo";
+
+// if (cat != "Oreo") {
+//     console.log("Oreo is a cat");
 // }
+// // else statement 
+// // and this is a comment 
 // else {
 //     console.log("Oreo is a food");
 // }
 
-// var a = 2, b = 4, c = 6;
+var array = ["Ahanf", "Akib", "Humayra", "Fahmida"];
 
-// if (a + a == c && c - a == b) {
-//     console.log("the statements are true");
-// }
-// else if (a + b == c || a + c == b) {
-//     console.log("one of the statements are true");
-// }
-// else {
-//     console.log("the statements are wrong");
-// }
+arrayNumber = array.length;
+console.log(arrayNumber);
 
-var cat = "Oreo";
-
-if (cat != "Oreo") {
-    console.log("Oreo is a cat");
+if (arrayNumber < 3) {
+    console.log("You are alone");
 }
-// else statement 
-// and this is a comment 
+else if (arrayNumber > 5) {
+    console.log("You are too much crowded");
+}
 else {
-    console.log("Oreo is a food");
+    console.log("You are happy");
 }
---
+++ JavaScript/while-loop.js
@@ -0,0 +1,5 @@
+var days = 0;
+while (days == 5) {
+    console.log("You have won");
+    days++;
+}
