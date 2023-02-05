84d5ad481a31e07cabcad6d348f3301a22532d68
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-05 23:13:55 +0600

    b

--- JavaScript/convert.js
+++ JavaScript/convert.js
@@ -1,12 +1,31 @@
-function isMoonUp(time) {
-    if (time > 19 || time < 5) {
-        return true;
+// function isMoonUp(time) {
+//     if (time > 19 || time < 5) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// };
+
+// let clockTime = 11;
+// let moon = isMoonUp(clockTime);
+// console.log(moon);
+
+// odd number - even number
+
+function oddNumber(number) {
+    if (number % 2 == 0) {
+        console.log("the number is an even number");
+    }
+    else if (number % 2 != 0) {
+        console.log("the numebr is an odd number");
     }
     else {
-        return false;
+        console.log("the number is zero");
     }
 };
 
-let clockTime = 11;
-let moon = isMoonUp(clockTime);
-console.log(moon);
+let inputNumber = 3;
+oddNumber(inputNumber);
+// let result = oddNumber(inputNumber);
+// console.log(result);
