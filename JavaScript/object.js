57d5afdd41199193d9ebe1d82626ebb305f48e4b
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-05 07:04:59 +0600

    g

--- JavaScript/object.js
+++ JavaScript/object.js
@@ -1,14 +1,30 @@
-var phone = {
-    brand: "samsung",
-    model: 223,
-    price: 12000,
-    processor: "AMD"
+// var phone = {
+//     brand: "samsung",
+//     model: 223,
+//     price: 12000,
+//     processor: "AMD"
 
-};
-console.log(phone.brand);
+// };
+// console.log(phone.brand);
 
-phone.price = 8000;
-console.log(phone.price);
+// phone.price = 8000;
+// console.log(phone.price);
 
-phone["price"] = 9000;
-console.log(phone.price);
+// phone["price"] = 9000;
+// console.log(phone.price);
+
+var color = "skyBlue"
+
+switch (color) {
+    case "white": console.log("the color is white");
+        break;
+
+    case "green": console.log("the color is green");
+    break;
+
+    case "blue": console.log("the color is blue");
+    break;
+
+    default: console.log("the color is black");
+
+}
