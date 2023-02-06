22e2578c718858241808debd7c425e7b14f0b23c
Md-Ahanf-Wadud-Akib <ahnafakib009@gmail.com>
2023-02-06 07:07:26 +0600

    factorialWithFunction

--- JavaScript/convert.js
+++ JavaScript/convert.js
@@ -1,31 +1,53 @@
-// function isMoonUp(time) {
-//     if (time > 19 || time < 5) {
-//         return true;
+// // function isMoonUp(time) {
+// //     if (time > 19 || time < 5) {
+// //         return true;
+// //     }
+// //     else {
+// //         return false;
+// //     }
+// // };
+
+// // let clockTime = 11;
+// // let moon = isMoonUp(clockTime);
+// // console.log(moon);
+
+// // odd number - even number
+
+// function oddNumber(number) {
+//     if (number % 2 == 0) {
+//         console.log("the number is an even number");
+//     }
+//     else if (number % 2 != 0) {
+//         console.log("the numebr is an odd number");
 //     }
 //     else {
-//         return false;
+//         console.log("the number is zero");
 //     }
 // };
 
-// let clockTime = 11;
-// let moon = isMoonUp(clockTime);
-// console.log(moon);
+// let inputNumber = 3;
+// oddNumber(inputNumber);
+// // let result = oddNumber(inputNumber);
+// // console.log(result);
 
-// odd number - even number
 
-function oddNumber(number) {
-    if (number % 2 == 0) {
-        console.log("the number is an even number");
+// let factorial = 1;
+// let n = 7;
+
+// for (i = 1; i <= n; i++) {
+//     factorial = factorial * i;
+// }
+// console.log("the factorial of ", n, " is: ", factorial);
+
+function factorial(n) {
+    let sum = 1, i;
+    for (i = 1; i <= n; i++) {
+        sum = sum * i;
+
     }
-    else if (number % 2 != 0) {
-        console.log("the numebr is an odd number");
-    }
-    else {
-        console.log("the number is zero");
-    }
-};
+    return sum;
+}
 
 let inputNumber = 3;
-oddNumber(inputNumber);
-// let result = oddNumber(inputNumber);
-// console.log(result);
+let result = factorial(inputNumber);
+console.log("the result is:", result);
